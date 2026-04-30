036230d8642caad04a17a8cd091338468b777a9d
Mohan006007 <mohanakrishnan707@gmail.com>
2026-05-01 02:30:16 +0530

    add health endpoint and fix probes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import random
 from prometheus_client import start_http_server, Gauge
 
@@ -6,11 +6,17 @@ app = Flask(__name__)
 
 cpu_metric = Gauge('cpu_usage', 'Simulated CPU usage')
 
+# 🔥 Health endpoint (for Kubernetes)
+@app.route("/health")
+def health():
+    return "OK", 200
+
+# 🔥 App endpoint
 @app.route("/")
 def home():
     value = random.randint(10, 95)
     cpu_metric.set(value)
-    return {"cpu": value}
+    return jsonify({"cpu": value}), 200
 
 if __name__ == "__main__":
     start_http_server(8000)
